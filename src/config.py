8739f64b5d282e1f000d3eac775f3075e01b799e
DeepBlockDeepak <jordan.medina1729@gmail.commit>
2023-11-22 01:24:31 -0700

    [WIP] modified main and modularized the login routing function

---
+++ src/config.py
@@ -0,0 +1,32 @@
+import os
+
+
+class Config:
+	"""Base configuration."""
+
+	# Default secret key
+	SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
+
+	# Default SQLAlchemy settings
+	SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///travel_library.db")
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+	"""Development configuration."""
+
+	DEBUG = True
+	SQLALCHEMY_ECHO = True  # If you want to see SQLAlchemy queries in the logs
+
+
+class TestingConfig(Config):
+	"""Testing configuration."""
+
+	TESTING = True
+	SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"  # Use in-memory SQLite for tests
+
+
+class ProductionConfig(Config):
+	"""Production configuration."""
+
+	DEBUG = False
