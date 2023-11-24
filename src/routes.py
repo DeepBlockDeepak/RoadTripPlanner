468562281ab080eba9338e82928b2dc6657a09ec
DeepBlockDeepak <jordan.medina1729@gmail.commit>
2023-11-23 17:58:38 -0700

    Created the 'auth' blueprint directory. 'login', 'register', and 'logout' routes were removed from src/routes.py to the src/auth/routes.py and imports were updated accordingly. the /main.py logic is now inside of src/__init__.py. /main.py will likely be removed next commit.

--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,11 @@
 import json
 
 from flask import flash, redirect, render_template, request, session, url_for
-from flask_login import current_user, login_required, login_user, logout_user
+from flask_login import current_user, login_required
 
-from main import app  # @ BUG: Deprecated!!!
+from run import app  # @ BUG: Deprecated!!!
 from database import db
-from src.forms import BudgetForm, LoginForm, OriginDestinationForm, RegistrationForm
+from src.forms import BudgetForm, OriginDestinationForm
 from src.map_requests import APIError, get_cities_list
 from src.models import (
 	Blurb,
@@ -29,54 +29,11 @@ from src.routing_helper_functions import (
 	obtain_travel_price,
 	parse_travel_form_data,
 	place_generator,
-	authenticate_user,
 )
 from src.scraping_functions.wiki_places import get_main_image
 
 # constant; key for the session to store the logged-in user-id
 CURRENT_SESSION_USER = "current_session_user"
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-	# Redirect already authenticated users to the home page
-	if current_user.is_authenticated:
-		flash("You are already logged in.")
-		return redirect(url_for("welcome_page"))
-
-	# Create and process the login form
-	form = LoginForm(csrf_enabled=False)
-	if form.validate_on_submit():
-		# Authenticate user
-		user = authenticate_user(form.email.data, form.password.data)
-		if user:
-			# Login user and set session variable
-			login_user(user, remember=form.remember.data)
-			session[CURRENT_SESSION_USER] = user.id
-
-			# Redirect to the requested page or to the user's profile
-			next_page = request.args.get("next")
-			flash(f"Welcome back, {user.username}!")
-			return redirect(next_page or url_for("profile", user_id=user.id))
-		flash("Invalid username or password.")
-
-	return render_template("login.html", form=form)
-
-
-# handles logout functionality
-@app.route("/logout")
-@login_required
-def logout():
-	# find who the logged-in user is
-	logged_in_user_id = session[CURRENT_SESSION_USER]
-	logged_in_user = User.query.filter_by(id=logged_in_user_id).first_or_404(
-		description="No such user found."
-	)
-	logout_user()
-	# reset the current session user to None
-	session[CURRENT_SESSION_USER] = "None"
-	flash(f"Successfully logged out, {logged_in_user.username}.")
-	return render_template("welcome_page.html")
 
 
 # Renders page detailing the Place()
@@ -124,56 +81,6 @@ def travel_profile(user_id, travel_id):
 	return render_template(
 		"profile_travels.html", user=user, travel=travel, places=places
 	)
-
-
-# Routes the "REGISTER" navbar button to the registration form
-@app.route("/register", methods=["GET", "POST"])
-def register():
-	# immediately check if the user is logged in, and if so, redirect them
-	if current_user.is_authenticated:
-		flash("You must logout before you can register.")
-		return render_template("welcome_page.html")
-	# instance the registration form
-	form = RegistrationForm(csrf_enabled=False)
-
-	# upon valid Post submission create a new User with their associated new Lists!
-	if request.method == "POST" and form.validate_on_submit():
-		# create new User Lists and commit to DB
-		new_travel_list = Travellist()
-		new_fav_list = Favoritelist()
-		new_search_list = Searchlist()
-
-		db.session.add(new_travel_list)
-		db.session.add(new_fav_list)
-		db.session.add(new_search_list)
-		# MUST COMMIT BEFORE INSTANCING new USER
-		db.session.commit()
-
-		# define user with data from form here:
-		user = User(
-			username=form.username.data,
-			email=form.email.data,
-			budget=0,
-			travellist_id=new_travel_list.id,
-			favoritelist_id=new_fav_list.id,
-			searchlist_id=new_search_list.id,
-		)
-
-		# set user's password here and then add the new user to the db
-		user.set_password(form.password.data)
-
-		db.session.add(user)
-		db.session.commit()
-
-		# Log the Newly Registered User into the Travel App,
-		#     thereby allowing the login-protected profiles route!
-		login_user(user, remember=False)
-		session[CURRENT_SESSION_USER] = user.id
-
-		# a succesful submission redirects the new user's profile page
-		return redirect(url_for("profile", user_id=user.id))
-
-	return render_template("registration.html", title="Register", form=form)
 
 
 # Routes the "DB SCHEMA" navbar button to the image of our DB Schema
