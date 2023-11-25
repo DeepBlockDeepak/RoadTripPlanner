f85533b40c68dba395282363a2e45f9147fa790f
DeepBlockDeepak <jordan.medina1729@gmail.com>
2023-11-24 20:04:30 -0700

    Added documentation to the app's models in models.py

--- app/models.py
+++ app/models.py
@@ -12,44 +12,53 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from database import db
 
 
-# Allows a User to write a "tweet" on the Dashboard
 class Blurb(db.Model):
+	"""Represents a short message or 'tweet' that a user can post to the dashboard.
+
+	Attributes:
+	        id (int): Primary key.
+	        content (str): The content of the blurb, limited to 120 characters.
+	        author_id (int): Foreign key linking to the user who authored the blurb.
+	        author (User): Relationship linking to the User model.
+	"""
+
 	id = db.Column(db.Integer, primary_key=True)
 	content = db.Column(db.String(120), nullable=False)
 	author_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-
 	author = db.relationship("User", backref=db.backref("blurbs", lazy=True))
 
 
-# A User has a list of favorited Destinations, as well as a list of searched ones
 class User(db.Model, UserMixin):
+	"""Represents a user of the application, including their authentication and profile details.
+
+	Attributes:
+	        id (int): Primary key.
+	        username (str): The user's chosen username, unique among all users.
+	        budget (Numeric): The user's travel budget.
+	        favoritelist_id (int): Foreign key linking to the user's list of favorited Places.
+	        searchlist_id (int): Foreign key linking to the user's list of searched Places.
+	        travellist_id (int): Foreign key linking to the user's list of Travels.
+	        email (str): The user's email address, unique among all users.
+	        password_hash (str): Hash of the user's password for secure storage.
+	        joined_at (DateTime): Timestamp when the user joined, in Mountain Time.
+
+	Methods:
+	        set_password(password): Hashes a password and stores it.
+	        check_password(password): Verifies a given password against the stored hash.
+	"""
+
 	id = db.Column(db.Integer, primary_key=True)
-
 	username = db.Column(db.String(50), index=True, unique=True)
-
-	# stores the user's budget
 	budget = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
-
-	# keep a list of favorited Destinations
 	favoritelist_id = db.Column(db.Integer, db.ForeignKey("favoritelist.id"))
-
-	# keep a list of searched destinations
 	searchlist_id = db.Column(db.Integer, db.ForeignKey("searchlist.id"))
-
-	# a list of Travels for each User
 	travellist_id = db.Column(db.Integer, db.ForeignKey("travellist.id"))
-
-	# user-sign-in/registration attributes
 	email = db.Column(db.String(120), index=True, unique=True)
-
 	password_hash = db.Column(db.String(128))
-
-	# format the joined_at to use Mountain Time
 	joined_at = db.Column(
 		db.DateTime(), index=True, default=datetime.now(pytz.timezone("America/Denver"))
 	)
 
-	# pass word hashing and authentication functions
 	def set_password(self, password):
 		self.password_hash = generate_password_hash(password)
 
@@ -61,21 +70,27 @@ class User(db.Model, UserMixin):
 		return f"{self.username}"
 
 
-# Each City will be modeled here
 class Place(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
+	"""Represents a City or geographic location.
 
+	Attributes:
+	        id (int): Primary key.
+	        city (str): Name of the city.
+	        state (str): Name of the state.
+	        population (int): Population of the place.
+	        activities (str): String of activities available at the place.
+	        wiki (str): Wiki information about the place.
+	        times_favorited (int): Number of times this place has been favorited.
+	        times_searched (int): Number of times this place has been searched.
+	"""
+
+	id = db.Column(db.Integer, primary_key=True)
 	city = db.Column(db.String(80), index=True, unique=False)
 	state = db.Column(db.String(80), index=True, unique=False)
 	population = db.Column(db.Integer, index=False, unique=False)
-
-	# maybe semicolon delimit the activities, and parse them later on, hence a larger String value here
 	activities = db.Column(db.String(1000), index=False, unique=False)
-
 	wiki = db.Column(db.String(5000), index=False, unique=False)
-
 	times_favorited = db.Column(db.Integer, index=False, unique=False)
-
 	times_searched = db.Column(db.Integer, index=False, unique=False)
 
 	# custom representation for html
@@ -83,12 +98,17 @@ class Place(db.Model):
 		return f"{self.city}, {self.state}"
 
 
-# creates a union of a Favorited Place and the the Favorite list it is associated with
 class Favoriteitem(db.Model):
+	"""Links a Place to a User's list of favorites, representing a favorited place.
+
+	Attributes:
+	        id (int): Primary key.
+	        place_id (int): Foreign key to the associated Place.
+	        favoritelist_id (int): Foreign key to the user's Favoritelist.
+	"""
+
 	id = db.Column(db.Integer, primary_key=True)
-
 	place_id = db.Column(db.Integer, db.ForeignKey("place.id"))
-
 	favoritelist_id = db.Column(db.Integer, db.ForeignKey("favoritelist.id"))
 
 	def __repr__(self):
@@ -98,20 +118,32 @@ class Favoriteitem(db.Model):
 
 
 class Searchitem(db.Model):
+	"""Links a Place to a User's list of searched items, representing a searched Place.
+
+	Attributes:
+	        id (int): Primary key.
+	        place_id (int): Foreign key to the associated Place.
+	        searchlist_id (int): Foreign key to the User's Searchlist.
+	"""
+
 	id = db.Column(db.Integer, primary_key=True)
-
 	place_id = db.Column(db.Integer, db.ForeignKey("place.id"))
-
 	searchlist_id = db.Column(db.Integer, db.ForeignKey("searchlist.id"))
 
 
 # Like the other *item classes, this class is for abstracting a Place() with the list Class that stores it.
 # That way, if a 'place' needs to ever be removed from a list, the Place() entity itself isn't removed from the db, just the abstracted item class.
 class Travelplaceitem(db.Model):
+	"""Links a Place to a Travel itinerary, representing a stop or destination on a Travel route.
+
+	Attributes:
+	        id (int): Primary key.
+	        place_id (int): Foreign key to the associated place.
+	        travel_id (int): Foreign key to the associated travel itinerary.
+	"""
+
 	id = db.Column(db.Integer, primary_key=True)
-
 	place_id = db.Column(db.Integer, db.ForeignKey("place.id"))
-
 	travel_id = db.Column(db.Integer, db.ForeignKey("travel.id"))
 
 
@@ -124,10 +156,15 @@ class Travelplaceitem(db.Model):
 """
 
 
-# acts as a list of Favorited places
 class Favoritelist(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
+	"""Represents a User's list of favorited Places.
 
+	Attributes:
+	        id (int): Primary key.
+	        favoriteplaces (relationship): Dynamically loaded list of Favoriteitems.
+	"""
+
+	id = db.Column(db.Integer, primary_key=True)
 	favoriteplaces = db.relationship(
 		"Favoriteitem",
 		backref="favoritelist",
@@ -136,10 +173,15 @@ class Favoritelist(db.Model):
 	)
 
 
-# acts as a list of Searched places
 class Searchlist(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
+	"""Represents a User's list of searched Places.
 
+	Attributes:
+	        id (int): Primary key.
+	        searchplaces (relationship): Dynamically loaded list of Searchitems.
+	"""
+
+	id = db.Column(db.Integer, primary_key=True)
 	searchplaces = db.relationship(
 		"Searchitem",
 		backref="searchlist",
@@ -148,19 +190,22 @@ class Searchlist(db.Model):
 	)
 
 
-# Travel class belonging to A User who Submitted an Origin/Destination request.
 class Travel(db.Model):
+	"""Represents a User's travel plan, including origin, destination, and route.
+
+	Attributes:
+	        id (int): Primary key.
+	        origin_place_id (int): Foreign key to the Place of origin.
+	        destination_place_id (int): Foreign key to the destination Place.
+	        travellist_id (int): Foreign key to the user's Travellist.
+	        price (Numeric): Estimated cost of the travel.
+	        route_places (relationship): Dynamically loaded list of Places on the travel route.
+	"""
+
 	id = db.Column(db.Integer, primary_key=True)
-
-	# origin and dest Places are pointed to
 	origin_place_id = db.Column(db.Integer, db.ForeignKey("place.id"))
-
 	destination_place_id = db.Column(db.Integer, db.ForeignKey("place.id"))
-
-	# Travel belongs a specific User's specific TravelList
 	travellist_id = db.Column(db.Integer, db.ForeignKey("travellist.id"))
-
-	# stores the expense of this particular road-trip/Travel
 	price = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
 
 	# 1 Travel() -> Many TravelPlaceItem()'s where each TravelPlaceItem has a foreign key to the Travel entity and a Place entity
@@ -178,10 +223,15 @@ class Travel(db.Model):
 		return f"{origin_place}   --->   {dest_place}"
 
 
-# One User's Travellist can hold many Travels
 class Travellist(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
+	"""Represents a User's list of Travel plans.
 
+	Attributes:
+	        id (int): Primary key.
+	        travels (relationship): Dynamically loaded list of Travel objects.
+	"""
+
+	id = db.Column(db.Integer, primary_key=True)
 	travels = db.relationship(
 		"Travel",
 		backref="travellist",
